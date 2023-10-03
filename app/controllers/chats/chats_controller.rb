62c22c39be0908c65d996a3984e075459dbf8351
greglobrigo <golivefun@gmail.com>
2023-10-04 04:04:19 +0800

    Added archive chat controller.

--- app/controllers/chats/chats_controller.rb
+++ app/controllers/chats/chats_controller.rb
@@ -77,6 +77,19 @@ class Chats::ChatsController < ApplicationController
     render json: { status: "success", message: 'Chat created successfully', chat_id: chat.id, messages: [] }, status: :ok
   end
 
+  def archive_chat
+    request_body = JSON.parse(request.body.read)
+    chat_id = request_body['chat_id']
+    user_id = request_body['user_id']
+    chat = Chat.joins(:chat_members).where(chat_members: {user_id: user_id}).where(id: chat_id).first
+    if chat.nil?
+      render json: { status: 'failed', error: 'Chat not found' }, status: :not_found
+    else
+      Chat.update(chat.id, archived: true)
+      render json: { status: "success", message: 'Chat archived successfully' }, status: :ok
+    end
+  end
+
   def delete
     request_body = JSON.parse(request.body.read)
     chat_id = request_body['chat_id']
