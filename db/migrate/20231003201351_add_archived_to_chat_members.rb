bd42bfc534c61558f713467604ce79b52d672621
greglobrigo <golivefun@gmail.com>
2023-10-04 04:15:59 +0800

    Added archived column to schema.

--- db/migrate/20231003201351_add_archived_to_chat_members.rb
+++ db/migrate/20231003201351_add_archived_to_chat_members.rb
@@ -1,7 +1,5 @@
 class AddArchivedToChatMembers < ActiveRecord::Migration[7.0]
   def change
-    add_column :chat_members, :archived, :boolean
-    add_column :chat_members, :default, :string
-    add_column :chat_members, :false, :string
+    add_column :chat_members, :archived, :boolean, default: false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_01_135925) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_03_201351) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pgcrypto"
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_01_135925) do
     t.integer "last_read_message_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "archived", default: false
   end
 
   create_table "chats", force: :cascade do |t|
