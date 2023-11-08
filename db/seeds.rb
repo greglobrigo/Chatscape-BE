4904db79ddd79bf6cbb065676ae68c281816b20b
greglobrigo <golivefun@gmail.com>
2023-11-09 05:20:49 +0800

    Added helper functions in seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,17 +7,37 @@
 #   Character.create(name: "Luke", movie: movies.first)
 
 # Create users
-# Avatar 1,2 and 3 are male avatars
-# Avatar 4,5 and 6 are female avatars
+
+def generateRandomMaleAvatar()
+    return rand(1..3)
+end
+
+def generateRandomFemaleAvatar()
+    return rand(4..6)
+end
+
 user1 = User.create!(
     id: SecureRandom.uuid,
     email: "johnsmith@gmail.com",
-    password: "password" + ENV["SALT"],
+    password: Base64.encode64("password" + ENV["SALT"]),
     name: "John S",
     handle: "@johnsmith",
     auth_token: "xyz123",
     status: "active",
     avatar: 1,
+    created_at: Time.now,
+    updated_at: Time.now
+)
+
+user2 = User.create!(
+    id: SecureRandom.uuid,
+    email: "brianmolina@gmail.com",
+    password: Base64.encode64(SecureRandom.alphanumeric(8) + ENV["SALT"]),
+    name: "Brian M",
+    handle: "@brianmolina",
+    auth_token: "xyz123",
+    status: "active",
+    avatar: generateRandomMaleAvatar(),
     created_at: Time.now,
     updated_at: Time.now
 )
