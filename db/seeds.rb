89249992b1f7e46b57838c3a2c73256a96f91fd6
greglobrigo <golivefun@gmail.com>
2023-11-09 05:12:58 +0800

    Added demo user in seedsrb.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+# Create users
+# Avatar 1,2 and 3 are male avatars
+# Avatar 4,5 and 6 are female avatars
+user1 = User.create!(
+    id: SecureRandom.uuid,
+    email: "johnsmith@gmail.com",
+    password: "password" + ENV["SALT"],
+    name: "John S",
+    handle: "@johnsmith",
+    auth_token: "xyz123",
+    status: "active",
+    avatar: 1,
+    created_at: Time.now,
+    updated_at: Time.now
+)
